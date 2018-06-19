a39b1e480499c19eee1e7f341964f7c94d6cdbad
Tomaz Muraus <tomaz@tomaz.me>
2018-06-19 18:11:28 +0200

    Fix code so it works under Python 3.

--- st2auth_ldap_backend/__init__.py
+++ st2auth_ldap_backend/__init__.py
@@ -13,7 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ldap_backend import LDAPAuthenticationBackend
+from __future__ import absolute_import
+
+from .ldap_backend import LDAPAuthenticationBackend
 
 __all__ = [
     'LDAPAuthenticationBackend'
